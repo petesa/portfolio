0c9aefdc2b4d5793c6cf414fb39f2d80d1e97b91
Pedro Sánchez Lira <pedro.sanchezlira@gmail.com>
2023-05-12 21:09:49 +0200

    Create a very simple template for the portfolio, with a basic layout and functionality

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+const pageNum = 3;
+const content = document.querySelector('.content');
+var page = Math.floor(Math.random() * pageNum);
+var height = window.innerHeight;
+
+window.addEventListener('resize', adjustHeight)
+document.querySelector('.prev').addEventListener('click', goToPrev);
+document.querySelector('.next').addEventListener('click', goToNext);
+
+updatePage();
+
+function adjustHeight() {
+    height = window.innerHeight;
+}
+
+function goToPrev(event) {
+    event.preventDefault();
+    if (page > 0) {
+        page -= 1;
+    } else {
+        page = pageNum - 1;
+    }
+    updatePage();
+}
+
+function goToNext(event) {
+    event.preventDefault();
+    if (page < pageNum - 1) {
+        page += 1;
+    } else {
+        page = 0;
+    }
+    updatePage();
+}
+
+function updatePage() {
+    content.scrollTo(0, page * height);
+}
